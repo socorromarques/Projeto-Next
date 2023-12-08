576cff50f00a6649c98fe09fc8bb3020ffea03b7
socorro marques <msocorrosmarques@gmail.com>
2023-12-08 14:12:16 -0300

    definindo tipagem

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,12 +1,14 @@
-function NavBar ( {
+import Link from "next/link";
+function NavBar() {
     return (
         <nav className='fixed top-0 w-full flex items-center py-2 p-x-8 justify-between z-50 bg-slate-800 text-gray-50'>
-        <link
+        <Link
         href='/' 
         className='uppercase font-bold text-ml h-12 flex items-center'
         >
           NEXT STORE
-        </link>
+        </Link>
        </nav>
-    )
-})
+   ) 
+}
+export default NavBar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+Async function getProducts() {
+  const res = await fetch('https://fakestoreapi.com/products');
+  if(!res) {
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+  
+}
+
+export default async function Home() {
+  const products = await getProducts();
   return (
-   <div>
-    <h1>Projeto Next</h1>
+   <div className="max-w-7xl mx-auto pt-8 px-8 xl-px-0">
+   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+    {products.map((product) => (
+      <div key={product.id}>{product.title}</div>
+     
+    ))}
+   </div>
    </div>
   )
   }
